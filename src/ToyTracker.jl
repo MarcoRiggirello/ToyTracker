20f4a2fb0442c94cd64b02a0f10bbc7433166da3
Marco Riggirello <marco.riggirello@cern.ch>
2025-03-10 00:44:45 +0100

    added intersection plane - straight track

--- src/ToyTracker.jl
+++ src/ToyTracker.jl
@@ -7,10 +7,11 @@ greet() = print("Toy model of a particle tracker to create toy MC for alignment 
 
 include("geometry_types.jl")
 include("track_types.jl")
-include("display.jl")
+include("detector_types.jl")
 #include("utils.jl")
 include("geometry.jl")
-#include("intersection.jl")
+include("intersection.jl")
+include("display.jl")
 
 export LocalCoordinates, GlobalCoordinates
 export LocalDirection, GlobalDirection
--- src/intersection.jl
+++ src/intersection.jl
@@ -1,57 +1,13 @@
 # Marco Riggirello
 
-function project(t::ParticleTrack, s::PlacedSensor)
-    M = position(s)
-    d = direction(t)
-    p = position0(t)
-    return ProjectedTrack(M\d, M\p)
-end
-
-
-function intersection(t::ProjectedTrack)
-    d = direction(t)
-    p = position0(t)
-    if d.w == 0
-        @info "The detector plane is parallel to the track."
-        return missing
-    else
-        τ = -(p.w/d.w)
-        return t(τ)
+function intersection(t::StraightTrack, p::Pose)
+    dir = t.direction
+    pos = t.position
+    l_dir = p(dir)
+    l_pos = p(pos)
+    if iszero(l_dir.w)
+        @warn "The detector plane is parallel to the track."
     end
+    τ = -l_pos.w / l_dir.w
+    return t(τ)
 end
-
-
-function interaction(t::ProjectedTrack, s::SiliconSensor)
-    sz_x, sz_y, _ = sensordims(s)
-    px_x, px_y = pixeldims(s)
-    ip_x, ip_y, _ = intersection(t)
-
-    if !(0 < ip_x < sz_x) || !(0 < ip_y < sz_y)
-        return missing
-    end
-
-    x = (ip_x ÷ px_x) * px_x + px_x/2
-    y = (ip_y ÷ px_y) * px_y + px_y/2
-
-    return Hit(Cluster(x, px_x), Cluster(y, px_y))
-end
-
-
-function interaction(t::ParticleTrack, s::PlacedSensor)
-    tt = project(t, s)
-    ss = sensor(s)
-    return interaction(tt, ss)
-end
-
-
-function interaction(t::ParticleTrack, d::Tracker)
-    hits = Dict{String, Union{Hit, Missing}}()
-    for s in sensors(d) 
-        id = s.first
-        ss = s.second
-        ht = interaction(t, ss)
-        push!(hits, id => ht)
-    end
-    return hits
-end
-
