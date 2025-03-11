848f97c29b6c48a5f7eb5582031914195d020881
Marco Riggirello <marco.riggirello@cern.ch>
2025-03-11 22:58:03 +0100

    bugfix

--- src/display.jl
+++ src/display.jl
@@ -13,28 +13,44 @@ end
 MakieCore.convert_arguments(P::Type{<:Lines}, t::AbstractParticleTrack, τ_range::AbstractRange) = tracklines(t, τ_range)
 
 function generate_mesh_faces(Nu, Nv)
-    if Nu < 3 || Nv < 3
-        throw(ArgumentError("Mesh grid can't be less than 3 by 3 points. Sorry."))
-    end
-    # This is honestly black magic
-    # Faces on the positive side of detector (triangular)
-    positive_indices_1 = [i + j for i in 1:2:(2Nu-2) for j in 0:2Nv:2(2Nv-1)]
-    positive_faces_1 = [i + k for i in positive_indices_1, k in (0, 2Nv, 2)]
-    positive_indices_2 = [i + j for i in 3:2:2Nu for j in 0:2Nv:2(2Nv-1)]
-    positive_faces_2 = [i + k for i in positive_indices_2, k in (0, 2Nv - 2, 2Nv)]
+    # for how vertices are constructed their numeration
+    # is as follow (u is on the x axis, v on the y axis)
+    # positive side is
+    # … -   …   -   …   - …
+    # |     |       |     |
+    # 3 - 2Nv+3 - 4Nv+3 - …
+    # |     |       |     |
+    # 1 - 2Nv+1 - 4Nv+1 - …
+    # negative is like this + 1.
+    # Hence to make triangular faces to do mesh we
+    # need to move around the grid as obtained through
+    # these next, black magic gathered, ranges.
+    i_range = 1:2:2(Nv-1)
+    j_range = 0:2Nv:2Nv*(Nu-1)-1
+    # Faces on the positive side of detector
+    positive_indices = [i + j for i in i_range for j in j_range]
+    # Lower triangles
+    positive_faces_1 = [i + k for i in positive_indices, k in (0, 2Nv, 2)]
+    # Upper triangles
+    positive_faces_2 = [i + k for i in positive_indices, k in (2Nv, 2Nv + 2, 2)]
+    # Total
     positive_faces = [positive_faces_1; positive_faces_2]
-    # Faces on the negative side of detector (triangular)
-    negative_indices_1 = [i + j for i in 2:2:(2Nu-2) for j in 0:2Nv:2(2Nv-1)]
-    negative_faces_1 = [i + k for i in negative_indices_1, k in (0, 2, 2Nv)]
-    negative_indices_2 = [i + j for i in 4:2:2Nu for j in 0:2Nv:2(2Nv-1)]
-    negative_faces_2 = [i + k for i in negative_indices_2, k in (0, 2Nv, 2Nv - 2)]
+    # Faces on the negative side of detector
+    # Lower triangles
+    negative_indices = [i + j + 1 for i in i_range for j in j_range]
+    negative_faces_1 = [i + k for i in negative_indices, k in (0, 2, 2Nv)]
+    # Upper triangles
+    negative_faces_2 = [i + k for i in negative_indices, k in (2Nv + 2, 2, 2Nv)]
+    # Total
     negative_faces = [negative_faces_1; negative_faces_2]
-    # total
+    # Now the edges of the sensor. The black magic
+    # is of the same kind, mutatis mutandis.
+    # Grand total
     return [positive_faces; negative_faces]
 end
 
 function local_sensor_mesh(s::IdealSensor)
-    Nu, Nv = 3, 3
+    Nu, Nv = 2, 2
     Lu, Lv = sensorsize(s)
     urange = LinRange(-Lu / 2, Lu / 2, Nu)
     vrange = LinRange(-Lv / 2, Lv / 2, Nv)
